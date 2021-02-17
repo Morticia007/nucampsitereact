31dd085cc1588d2d6e333e79854b4cf8e0a6e3f3
Morticia007 <jacquelyn07@mac.com>
2021-02-17 15:33:00 -0600

    Add redux thunk

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -10,6 +10,7 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { LocalForm, Errors, Control } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -156,6 +157,26 @@ function RenderCampsite({ campsite }) {
     } 
 
 function CampsiteInfo(props) {
+  if (props.isLoading) {
+      return (
+          <div className="container">
+              <div className="row">
+                  <Loading />
+              </div>
+          </div>
+      );
+  }
+  if (props.errMess) {
+      return (
+          <div className="container">
+              <div className="row">
+                  <div className="col">
+                      <h4>{props.errMess}</h4>
+                  </div>
+              </div>
+          </div>
+      );
+  } 
   if (props.campsite) {
     return (
       <div className='container'>
@@ -180,12 +201,10 @@ function CampsiteInfo(props) {
           </div>
    </div>
   );
- 
- } else {
-   return <div/>
-
- }
+} 
+else {return <div/>
+}
 };
 
-
+ 
 export default CampsiteInfo;
