853a84db803f711fc1a58987caf20dcb30027d83
Morticia007 <jacquelyn07@mac.com>
2021-02-19 20:29:49 -0600

    Add React Animation Components

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -12,7 +12,7 @@ import { Link } from 'react-router-dom';
 import { LocalForm, Errors, Control } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -123,21 +123,24 @@ function RenderComments({ comments, postComment,campsiteId }) {
 if (comments) {
    return (
       <div className=' col-md-5 m-1'>
-        <h4>Comments</h4>
+                <h4>Comments</h4>
+                <Stagger in>
+                    {
+                        comments.map(comment => {
+                            return (
+                                <Fade in key={comment.id}>
+                                    <div>
+                                        <p>
+                                            {comment.text}<br />
+                                            -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                        </p>
+                                    </div>
+                                </Fade>
+                            );
+                        })
+                    }
+                </Stagger>
 
-        {comments.map((comment) => {
-           
-          return (
-            <div key= {comment.id}>
-              {comment.text}
-              <br/>
-            -- {comment.author},{' '}
-            {new Intl.DateTimeFormat('en-US', {year:'numeric',month:'short',day:'2-digit'}).format(new Date(Date.parse(comment.date)))}
-              <br/>
-              <br/>
-           </div>
-            );
-          })}
        <CommentForm campsiteId={campsiteId} postComment={postComment} />
       </div>
            ) 
@@ -146,14 +149,20 @@ if (comments) {
 }     
 function RenderCampsite({ campsite }) {
   return (
-    <div className='col-md-5 m-1'>
-      <Card>
-      <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-        <CardBody>
-          <CardText>{campsite.description}</CardText>
-        </CardBody>
-      </Card>
-    </div>
+    <div className="col-md-5 m-1">
+    <FadeTransform
+        in
+        transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
+        <Card>
+            <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+            <CardBody>
+                <CardText>{campsite.description}</CardText>
+            </CardBody>
+        </Card>
+    </FadeTransform>
+</div>
   );
 
     } 
